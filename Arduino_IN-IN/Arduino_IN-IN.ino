61ccbb0cb9a1c2d4a255775dd3dde1ce82f6f574
MaxiMax07 <max@MacBook-Pro-de-MAX.local>
2016-02-13 01:28:15 -0500

    Ajout de la fonction arrêt

--- Arduino_IN-IN/Arduino_IN-IN.ino
+++ Arduino_IN-IN/Arduino_IN-IN.ino
@@ -1,8 +1,8 @@
   // Nom pour les pin directionnel du motorshield
-const unsigned char M1DIR = 7;
-const unsigned char M2DIR = 8;
-const unsigned char M1PWM = 9;
-const unsigned char M2PWM = 10;
+const unsigned char AIN1 = 7;
+const unsigned char BIN1 = 8;
+const unsigned char AIN2 = 9;
+const unsigned char BIN2 = 10;
 const unsigned char LED = 13;
   // Déclaration des variables
 int vitesse;
@@ -16,10 +16,10 @@ void setup() {
 
   //On setup toute les pins directionnelles et le LED
   Serial.begin(9600);
-  pinMode(M1PWM, OUTPUT);
-  pinMode(M2PWM, OUTPUT);
-  pinMode(M1DIR, OUTPUT);
-  pinMode(M2DIR, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN2, OUTPUT);
   pinMode(LED, OUTPUT);
 }
 
@@ -40,55 +40,54 @@ void loop() {
 
   //Fonctions pour le déplacement
 
-  //Fonction de base
-void deplacement(int vitesse,int DIR1,int DIR2,int delai) {
-    //Valeurs de max et min en analogue
-  if (vitesse>=255) vitesse = 255;
-    //Valeur nécessairement positif puisque analogue (8 bit)
-  if (vitesse<0) vitesse=-vitesse;
-  // Direction et vitesse moteur 1
-  analogWrite(M1PWM,vitesse);
-  digitalWrite(M1DIR,DIR1);
-  // Direction et vitesse moteur 2
-  analogWrite(M2PWM,vitesse);
-  digitalWrite(M2DIR,DIR2);
-  // Temps de la procédure
-  delay(delai);
-}
   //Fonction avancer
 void avancer(int vitesse,int delai) {
-  // Sens vers l'avant
-  DIR1=HIGH;
-  DIR2=HIGH;
-  deplacement(vitesse,DIR1,DIR2,delai);
+   digitalWrite(AIN1,1);
+   digitalWrite(BIN1,1);
+   analogWrite(AIN2,vitesse);
+   analogWrite(BIN2,vitesse);
+   delay(delai);
 }
  //Fonction reculer
 void reculer(int vitesse,int delai) {
-  // Sens vers l'arrière
-  DIR1=LOW;
-  DIR2=LOW;
-  deplacement(vitesse,DIR1,DIR2,delai);
+  if (vitesse<0) vitesse=-vitesse;
+   digitalWrite(AIN2,1);
+   digitalWrite(BIN2,1);
+   analogWrite(AIN1,vitesse);
+   analogWrite(BIN1,vitesse);
+   delay(delai);
 }
   //Fonction rotation à droite
 void rotationD(int vitesse, int delai) {
-  // Sens mixe pour tourner vers la droite
-  DIR1=HIGH;
-  DIR2=LOW;
-  deplacement(vitesse,DIR1,DIR2,delai);
+   digitalWrite(AIN1,1);
+   digitalWrite(BIN2,1);
+   analogWrite(AIN2,vitesse);
+   analogWrite(BIN1,vitesse);
+   delay(delai);
 }
   //Fonction rotation à gauche
 void rotationG(int vitesse, int delai) {
-  // Sens mixe pour tourner vers la gauche
-  DIR1=LOW;
-  DIR2=HIGH;
-  deplacement(vitesse,DIR1,DIR2,delai);
+   digitalWrite(BIN1,1);
+   digitalWrite(AIN2,1);
+   analogWrite(BIN2,vitesse);
+   analogWrite(AIN1,vitesse);
+   delay(delai);
 }
   //Freiner
 void brake(int delai) {
-  DIR1=LOW;
-  DIR2=LOW;
-  vitesse=0;
-  deplacement(vitesse,DIR1,DIR2,delai);
+   digitalWrite(AIN1,1);
+   digitalWrite(BIN2,1);
+   analogWrite(AIN2,1);
+   analogWrite(BIN1,1);
+   delay(delai);
+}
+  //Coast, frein brutal
+void arret(int delai) {
+   digitalWrite(AIN1,0);
+   digitalWrite(BIN2,0);
+   analogWrite(AIN2,0);
+   analogWrite(BIN1,0);
+   delay(delai);
 }
 
 
--- README.txt
+++ README.txt
@@ -1,6 +1,10 @@
-	BUT DU PROJET:
+	IN/IN:
 
-À décrire.
+Plus compliqué à programmer. Mais permet de faire des arrêts et démarrage brutaux.
+
+	PHASE/ENABLE:
+
+Plus simple mais ne permet pas d’actions brutales et rapides.
 
 	FONCTIONS PRÊTES:
 
@@ -9,6 +13,7 @@ reculer(vitesse,delai);
 brake(delai);
 rotationD(vitesse, delai);
 rotationG(vitesse, delai);
+arret(delai);
 
 	VARIABLES MANIPULABLES:
 
@@ -17,9 +22,7 @@ delai => en miniseconde
 
 	À FAIRE:
 
--Comprendre si il faut mettre le mode IN/IN ou PHASE/ENABLE sur le arduino UNO!
--Programmer la fonction coast qui permet d’arrêter brutalement le véhicule
-(mais je ne sais pas comment)
+-Voir avec équipe utilité ajouté accélération brutale
 -Tester pour calibrer les temps de rotations VS angles.
 -Programmer les futures fonctions pour servomoteurs.
 -Tester les distances VS temps pour calibrer le robot.

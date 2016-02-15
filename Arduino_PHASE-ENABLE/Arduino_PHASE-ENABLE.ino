611b7a5b7e378b98a9acc26764d5c58bdddbf123
MaxiMax07 <maxi.colombie@gmail.com>
2016-02-15 18:03:42 -0500

    Corrections
    Le code est fonctionnel désormais!

--- Arduino_PHASE-ENABLE/Arduino_PHASE-ENABLE.ino
+++ Arduino_PHASE-ENABLE/Arduino_PHASE-ENABLE.ino
@@ -10,45 +10,12 @@ int delai;
 int DIR1;
 int DIR2;
 
-
-void setup() {
-  // put your setup code here, to run once:
-
-  //On setup toute les pins directionnelles et le LED
-  Serial.begin(9600);
-  pinMode(M1PWM, OUTPUT);
-  pinMode(M2PWM, OUTPUT);
-  pinMode(M1DIR, OUTPUT);
-  pinMode(M2DIR, OUTPUT);
-  pinMode(LED, OUTPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  // Cela sert à ramasser en continue les valeurs des capteurs
-
-  // Par exemple, le led jaune de base va s'allumer seulement
-  // lorsque le robot fait une rotation ou est à l'arrêt
-   //if (DIR1==HIGH && DIR2==LOW) {
-      //digitalWrite(LED, HIGH); }
-   //else if (DIR1==LOW && DIR2==HIGH) {
-      //digitalWrite(LED, HIGH); }
-   //else {
-      //digitalWrite(LED, LOW); }
-
-      //Clignote quand arduino tourne
-      digitalWrite(LED,HIGH);
-      delay(1000);
-      digitalWrite(LED,LOW);
-      delay(1000);
-}
-
   //Fonctions pour le déplacement
 
   //Fonction de base
 void deplacement(int vitesse,int DIR1,int DIR2,int delai) {
     //Valeurs max pour 3V estimer a 70 mais tests à faire encore
-  if (vitesse>=70) vitesse = 70;
+  if (vitesse>=130) vitesse = 130;
     //Valeur nécessairement positif puisque analogue (8 bit)
   if (vitesse<0) vitesse=-vitesse;
   // Direction et vitesse moteur 1
@@ -96,9 +63,40 @@ void brake(int delai) {
   deplacement(vitesse,DIR1,DIR2,delai);
 }
 
+void setup() {
+  // put your setup code here, to run once:
+
+  //On setup toute les pins directionnelles et le LED
+  Serial.begin(9600);
+  pinMode(M1PWM, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
+  pinMode(M1DIR, OUTPUT);
+  pinMode(M2DIR, OUTPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Cela sert à ramasser en continue les valeurs des capteurs
+
+  // Par exemple, le led jaune de base va s'allumer seulement
+  // lorsque le robot fait une rotation ou est à l'arrêt
+   //if (DIR1==HIGH && DIR2==LOW) {
+      //digitalWrite(LED, HIGH); }
+   //else if (DIR1==LOW && DIR2==HIGH) {
+      //digitalWrite(LED, HIGH); }
+   //else {
+      //digitalWrite(LED, LOW); }
+
+
+
 
     // Ici on écrit le code! Avec les fonctions avancer, reculer
     // brake, rotationD et rotationG sous la forme (vitesse,delai)
     // vitesse étant compris entre 0 et 255 (8bit)
     // delai étant en ms
 
+avancer(130,15000);
+digitalWrite(LED,HIGH);
+reculer(130,15000);
+}

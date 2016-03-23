3f80d8af7c1eb74ecc0dfe2780078a3d27deaeea
MaxiMax07 <maxi.colombie@gmail.com>
2016-03-22 23:15:26 -0400

    Améliration code stepper
    A ajouté, les rotations

--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -1,22 +1,51 @@
+#include <BasicStepperDriver.h>
+#include <DRV8834.h>
+// #include <Arduino.h> Nécessaire???
 #include <Servo.h>
 
 #define RAYON 58
 
-int pas=0;
+//Pin pour DRV8834
+#define MOTOR_STEPS 200
+#define DIR1 6
+#define STEP1 7
+#define DIR2 8
+#define STEP2 9
+// Microstep control pour DRV8834, connection en parallèle des deux pins sur les deux drivers
+#define MICROSTEP 2 //Le nombre de step de plus à faire pour un step (doit etre 2^n)
+#define M0 10
+#define M1 11
 
+//Initialisation des steppers moteurs
+DRV8834 Stepper1(MOTOR_STEPS, DIR1, STEP1, M0, M1);
+DRV8834 Stepper2(MOTOR_STEPS, DIR2, STEP2, M0, M1);
+
+//Pin pour servo
+#define PINCE 1
+#define BRAS 2
+
+//Initilisation des servos
 Servo pince;
 Servo bras;
 
-void distance(int d)
+void deplacement(int d)
 {
-  int pas=d/(RAYON*2*3,1416)*200;
+  int pas = d / (RAYON * 2 * 3, 1416) * 200 * MICROSTEP;
+  for (int i = 0; i < pas; i++)
+  {
+    Stepper1.move(1);
+    Stepper2.move(1);
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pince.attach(9, 600, 2400);
-  bras.attach(10);
-  
+  pince.attach(PINCE, 600, 2400);
+  bras.attach(BRAS, 600, 2400);
+  Stepper1.setRPM(30*MICROSTEP); //Vitesse en RPM
+  Stepper2.setRPM(30*MICROSTEP);
+  Stepper1.setMicrostep(MICROSTEP); //Initialisation du microstep
+  Stepper2.setMicrostep(MICROSTEP);
 }
 
 void loop() {
@@ -32,6 +61,8 @@ void loop() {
   bras.write(0);
   delay(1000);
   pince.write(0);
-distance(65); //Distance en mm
+  delay(3000);
+  deplacement(200); //Distance en mm
+
 
 }
